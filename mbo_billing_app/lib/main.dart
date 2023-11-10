4616b1973d4b560aef2c703f18255d11538d36ce
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-11-10 12:06:46 +0530

    Update main.dart

--- mbo_billing_app/lib/main.dart
+++ mbo_billing_app/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
       ),
       home: HomePage(),
